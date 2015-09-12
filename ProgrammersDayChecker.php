bb0913631c57426ebd45d1c5bfaac54aedc69bec
KNone <i@knone.net>
2015-09-13 00:30:37 +0300

    check the programmer's day

---
+++ ProgrammersDayChecker.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProgrammersDayChecker
+{
+    public function check()
+    {
+        $currentDay = new \DateTime('today');
+        $interval = new DateInterval('P255D');
+        
+        return $currentDay->sub($interval)->format('dm') === '0101';
+    }
+}
+
+$programmersDayChecker = new ProgrammersDayChecker();
+
+if ($programmersDayChecker->check()) {
+    echo 'This is programmer\'s Day';
+}
